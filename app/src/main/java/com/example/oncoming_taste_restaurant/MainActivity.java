66e4e939d4eec855ab2a3918f95adbcb3af258b2
kimd0nghun <37896242+kimd0nghun@users.noreply.github.com>
2020-01-14 18:11:00 +0900

    first

---
+++ app/src/main/java/com/example/oncoming_taste_restaurant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.oncoming_taste_restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
